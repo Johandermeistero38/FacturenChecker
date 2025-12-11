96e5ba324c77f94aec168dda4c943421a44604cc
Johandermeistero38 <johanvanbelen@gmail.com>
2025-12-11 15:43:47 +0100

    Update matcher.py

--- src/matrix/matcher.py
+++ src/matrix/matcher.py
@@ -67,17 +67,18 @@ def evaluate_rows(rows, supplier_key="toppoint", progress_callback=None):
                 plooi_prices[plooi] = "N/A"
                 continue
 
-            try:
-                print("DEBUG df type:", type(df))
-print("DEBUG index type:", type(df.index[0]))
-print("DEBUG col type:", type(df.columns[0]))
-print("DEBUG rounded_w:", rounded_w, type(rounded_w))
-print("DEBUG rounded_h:", rounded_h, type(rounded_h))
+           try:
+    print("DEBUG df type:", type(df))
+    print("DEBUG index type:", type(df.index[0]))
+    print("DEBUG col type:", type(df.columns[0]))
+    print("DEBUG rounded_w:", rounded_w, type(rounded_w))
+    print("DEBUG rounded_h:", rounded_h, type(rounded_h))
 
-                price = df.loc[rounded_h, rounded_w]
-            except Exception:
-                plooi_prices[plooi] = "N/A"
-                continue
+    price = df.loc[rounded_h, rounded_w]
+except Exception:
+    plooi_prices[plooi] = "N/A"
+    continue
+
 
             plooi_prices[plooi] = price
 
