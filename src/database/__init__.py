17a67a12ed532cfc9018fea5be3e444192fae800
Johandermeistero38 <johanvanbelen@gmail.com>
2025-12-11 16:37:05 +0100

    Create __init__.py

---
+++ src/database/__init__.py
@@ -0,0 +1,15 @@
+src/
+  database/
+    __init__.py     ← moet er zijn!
+    supplier_db.py
+  matrix/
+    __init__.py
+    matcher.py
+    matrix_loader.py
+  parser/
+    __init__.py
+    pdf_parser.py
+  utils/
+    __init__.py
+    rounding.py
+  app.py
